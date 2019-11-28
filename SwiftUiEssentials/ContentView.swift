7f006defda8d25b8d549ed0727f553c527172fd5
Alvarodobleg <38034098+Alvarodobleg@users.noreply.github.com>
2019-11-28 19:26:22 +0100

    Add files via upload
    Subida del proyecto

---
+++ SwiftUiEssentials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiEssentials
+//
+//  Created by DAM+DM-2019 on 28/11/2019.
+//  Copyright © 2019 DAM+DM-2019. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Worlds!").font(.title).foregroundColor(.pink)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
