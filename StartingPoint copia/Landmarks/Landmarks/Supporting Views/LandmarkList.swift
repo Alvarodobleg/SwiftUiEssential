3206cbf04ed0b5490947c172b61685b93dc46679
Alvaro <alvarodobleg@gmail.com>
2019-11-29 18:37:10 +0100

    Añadido el navégate para cambiar de pantalla al seleccionar la imagen

---
+++ StartingPoint copia/Landmarks/Landmarks/Supporting Views/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by DAM+DM-2019 on 29/11/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail()){
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
